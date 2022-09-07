886973a89b4337e387a726cce5ee665d1d189438
anshi06 <araghya6768@gmail.com>
2022-09-07 10:44:19 +0530

    update state using prev state

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -11,27 +11,40 @@ const ExpenseForm = ()=>{
     });
     const titleChangeHandler=(event)=>{
         // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            eneteredTitle : event.target.value,
+        // setUserInput({
+        //     ...userInput,
+        //     eneteredTitle : event.target.value,
+        // })
+        setUserInput((prevState) =>{
+            return {...prevState, enteredTitle : event.target.value}
+
         })
     }
 
    
     const amountChangeHandler=(event)=>{
         // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            eneteredAmount : event.target.value,
+        // setUserInput({
+        //     ...userInput,
+        //     eneteredAmount : event.target.value,
+        // })
+        setUserInput((prevState) =>{
+            return {...prevState, enteredAmount : event.target.value}
+
         })
     }
 
    
     const dateChangeHandler=(event)=>{
         // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            eneteredDate : event.target.value,
+        // setUserInput({
+        //     ...userInput,
+        //     eneteredDate : event.target.value,
+        // })
+
+        setUserInput((prevState) =>{
+            return {...prevState, enteredDate : event.target.value}
+
         })
     }
     return(
